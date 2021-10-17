f1e9dc25afec86e4c94a4520e6326eedde026c74
Carter Dooley <carter.j.dooley43@gmail.com>
2021-10-17 15:01:07 -0500

    Password Test 2

--- password.js
+++ password.js
@@ -9,6 +9,15 @@ reader.question("Please enter a valid Password.", function(input){
     tokens = input.split(' ')
 })
 
+passWord = tokens[0]
+
+
+  if (passWord.length < 10) {
+  console.log("Password is not long enough")
+}
+  if (passWord.length >= 10) {
+  console.log('This password is valid')
+}
 
 
 
@@ -16,16 +25,8 @@ reader.question("Please enter a valid Password.", function(input){
 
 
 
-
-// let password = "key"
-// let response
-
-
-// while(response !== password) {
-//     response = reader.question("Enter Password")
-// }
-
-// alert("This password works!")
+//Sources:
+//https://www.w3schools.com/howto/howto_js_password_validation.asp
 
 
 
